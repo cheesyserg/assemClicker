862eaf71be469264a5c4a9359068afad8a20d298
KDRN <42902433+cheesyserg@users.noreply.github.com>
2026-03-21 14:47:13 +0800

    Update build.bat to use ultra-brute compression

--- build.bat
+++ build.bat
@@ -4,7 +4,7 @@ ml.exe /c /coff clicker.asm
 
 :: 4096 is the "Magic Number" for Modern Windows
 link /SUBSYSTEM:WINDOWS /ENTRY:start /ALIGN:4096 /FILEALIGN:512 /MERGE:.rdata=.text /MERGE:.data=.text /SECTION:.text,EWR /NODEFAULTLIB clicker.obj gui.res user32.lib kernel32.lib 
-upx.exe --best clicker.exe
+upx.exe --ultra-brute clicker.exe
 
 echo Build Successful!
 pause
