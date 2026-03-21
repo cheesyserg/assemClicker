15b189655fa2dad3381370a810085f3ae637ad29
KDRN <42902433+cheesyserg@users.noreply.github.com>
2026-03-21 14:19:10 +0800

    Refactor build.bat for better alignment and compression
    Updated build script to improve alignment and add UPX compression.

--- build.bat
+++ build.bat
@@ -1,14 +1,10 @@
 @echo off
-echo Compiling resources...
 rc.exe gui.rc
-
-echo Assembling...
 ml.exe /c /coff clicker.asm
 
-echo Linking...
-:: Removed /MERGE:.rsrc=.text to fix LNK1272
-:: Set /ALIGN:32 to keep the file as small as possible 
-link /SUBSYSTEM:WINDOWS /ENTRY:start /ALIGN:32 /FILEALIGN:32 /MERGE:.rdata=.text /MERGE:.data=.text /SECTION:.text,EWR /FIXED /NODEFAULTLIB clicker.obj gui.res user32.lib kernel32.lib 
+:: 4096 is the "Magic Number" for Modern Windows
+link /SUBSYSTEM:WINDOWS /ENTRY:start /ALIGN:4096 /FILEALIGN:512 /MERGE:.rdata=.text /MERGE:.data=.text /SECTION:.text,EWR /NODEFAULTLIB clicker.obj gui.res user32.lib kernel32.lib 
+upx.exe --best clicker.exe
 
-echo Done!
-pause [cite: 2]
+echo Build Successful!
+pause
